4c68c89fa1924d13c500a415d245619cecf44c10
Evgeniy Bochegurov <e_boche@mail.ru>
2022-07-09 09:45:04 +0300

    Docker swarm gitlab CI/CD

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import connectionOptions from 'ormconfig';
+import { UserEntity } from './user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'api_db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [UserEntity],
+      synchronize: false,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
